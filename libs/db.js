19bdb9a78374492381d278417ca86cdcd61f042f
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-05-16 14:54:55 +0700

    connect to database

--- libs/db.js
+++ libs/db.js
@@ -1,3 +1,11 @@
+const sqlite3 = require('sqlite3');
+
 module.exports = (app, dbpath) => {
-    
+    app.db = new sqlite3.Database(dbpath, (err) => {
+        if (err) {
+            console.error(err.message);
+            process.exit(1);
+        }
+        console.log(`[${Date.now()}] Connected to the database.`);
+    });
 }
